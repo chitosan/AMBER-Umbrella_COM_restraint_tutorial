806f0910eb0aa2774acef0f239385f8f7addad83
Callum Dickson <callum_dickson@hotmail.com>
2016-03-07 22:31:09 -0500

    Added files via upload

---
+++ windows/run_window_cuda.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES="0"
+
+export prmtop=../DMPC_MOH.prmtop
+export name=DMPC_MOH
+
+for i in $(seq 32.0 -1.0 0.0)
+do
+
+mkdir dist_${i}
+cd ./dist_${i}
+
+cp ../COM_dist.RST .
+
+sed -i "s/DISTHERE/${i}/g" COM_dist.RST
+
+$AMBERHOME/bin/pmemd.cuda -O -i ../06_Prod.in -o 06_Prod_${name}_${i}.out -p $prmtop -c ../frame_${i}.rst -r 06_Prod_${name}_${i}.rst -x 06_Prod_${name}_${i}.nc -inf 06_Prod_${name}_${i}.mdinfo
+
+cd ../
+
+done
